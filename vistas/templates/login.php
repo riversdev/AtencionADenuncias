3534764e579550e45c4bdccd2c12ce961a2e9f4c
Paty242 <62078499+Paty242@users.noreply.github.com>
2020-10-09 12:34:58 -0500

    Login

--- vistas/templates/login.php
+++ vistas/templates/login.php
@@ -5,10 +5,11 @@ if (isset($_SESSION['user_id'])) {
     header("Location: /AtencionADenuncias/home");
 } else {
 ?>
+
     <div id="particles-js"></div>
     <div class="container contenedor" style="overflow-y: hidden;">
-        <div class="row align-items-center justify-content-center" style="height: 100vh;">
-            <div class="col-lg-7 col-sm-6 col-12">
+        <div class="row align-items-center justify-content-center" style="height: 100vh;">            
+            <!---<div class="col-lg-7 col-sm-6 col-12">
                 <div class="row align-items-center justify-content-start">
                     <div class="card border border-0 bg-transparent">
                         <div class="card-body">
@@ -16,17 +17,29 @@ if (isset($_SESSION['user_id'])) {
                         </div>
                     </div>
                 </div>
+            </div>--->
+            <div class="contact100-more flex-col-c-m col-lg-6 col-md-6 col-sm-12" style="color:white; max-width: 50%;max-height: 100%">
+                <div class="flex-w size1 p-b-47">
+                    <img src="vistas/static/img/logo_ofi.png" style="height: 14vh; padding-right: 150px">
+
+                    <img src="vistas/static/img/Escudo_de_Armas_Oficial_del_Estado_de_Hidalgo.png" style="height: 14vh;">
+                        <div class="card-text text-justify" style="padding-top: 30px; font-size: 20px; color:black"><span><strong> Atención a Denuncias por los Incumplimientos del Código de Ética de la Administración Pública del Estado de Hidalgo y Código de Conducta de Oficialía Mayor</strong></span></div>
+
+                 
+                </div>
+
             </div>
             <div class="col-lg-5 col-sm-6 col-12">
                 <div class="row align-items-center justify-content-end">
-                    <div class="card" style="max-width: 20rem;">
+                    <div class="card" style="max-width: 20rem; box-shadow: 5px 5px 10px gray; ">
                         <div class="card-body">
-                            <h5 class="card-title text-primary">Identifícate</h5>
+                            <h5 class="card-title text-primary" style="text-align:center">Identifícate</h5>
+
                             <div class="dropdown-divider"></div>
                             <form id="formIdentificarUsuario" class="needs-validation" novalidate>
                                 <div class="form-row">
                                     <div class="col-md-12 mb-3">
-                                        <label for="txtUsuario">Usuario</label>
+                                        <label for="txtUsuario"><i class="fa fa-user-circle "></i>  Usuario</label>
                                         <input type="text" class="form-control" id="txtUsuario" required>
                                         <div class="valid-feedback">
                                             Correcto!
@@ -36,7 +49,7 @@ if (isset($_SESSION['user_id'])) {
                                         </div>
                                     </div>
                                     <div class="col-md-12 mb-3">
-                                        <label for="txtPassword">Contraseña</label>
+                                        <label for="txtPassword"><i class="fa fa-key"></i> Contraseña</label>
                                         <input type="password" class="form-control" id="txtPassword" required>
                                         <div class="valid-feedback">
                                             Correcto!
@@ -124,7 +137,7 @@ if (isset($_SESSION['user_id'])) {
         width: 100%;
         position: fixed;
         z-index: -1;
-        background-color: #092432;
+        background-color: gray;
     }
 
     .contenedor {
